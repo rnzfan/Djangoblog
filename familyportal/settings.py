991a98b86173d13f906a523a4fa82fdaef2fc74c
rnzfan <87469911+rnzfan@users.noreply.github.com>
2021-11-01 09:44:52 +1300

    clear key comment

--- familyportal/settings.py
+++ familyportal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-g8g*rfra2d&)887$9m8gvdaw1vdh%bmff2orr5dms7*q8_bzf%'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
